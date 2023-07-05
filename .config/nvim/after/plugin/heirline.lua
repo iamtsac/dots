ed005504507bf7f2123a81d25626824abd5f2a1e
Constantinos Tsakonas <tsakonas.constantinos@gmail.com>
2023-07-05 16:11:09 +0300

    Nvim bar

--- .config/nvim/after/plugin/heirline.lua
+++ .config/nvim/after/plugin/heirline.lua
@@ -1,4 +1,4 @@
 local os = require "os"
-local utils = require("heirline.utils")
-local conditions = require("heirline.conditions")
+-- local utils = require("heirline.utils")
+-- local conditions = require("heirline.conditions")
 --require("heirline").setup(config)
