419d8cd3e58b324dec183dbeb0e307415dde012f
Constantinos Tsakonas <tsakonas.constantinos@gmail.com>
2024-02-28 16:48:24 +0200

    Updated nvim

---
+++ .config/nvim/nvim_/lua/configs/highlights.lua
@@ -0,0 +1,3 @@
+-- vim.highlight.create('LineNr', {ctermbg=0, guibg=false}, false)
+vim.api.nvim_set_hl(0, "LineNr", { bg = nil, ctermbg = nil, default = true })
+-- vim.cmd [[ highlight LineNr guibg=none ]]
