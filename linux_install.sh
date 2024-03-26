d24a50aca8293db26f15bcad5bc2462b22b27920
Constantinos Tsakonas <tsakonas.constantinos@gmail.com>
2024-03-26 13:49:27 +0200

    Tmux with sixel support installation

--- linux_install.sh
+++ linux_install.sh
@@ -9,7 +9,15 @@ sudo pacman -S --needed base-devel
 
 
 echo "Installing Essentials"
-sudo pacman -Sy neovim rustup fish tmux wezterm
+sudo pacman -Sy neovim rustup fish wezterm
+
+echo "Installing Tmux"
+git clone https://github.com/tmux/tmux.git
+cd tmux
+sh autogen.sh
+./configure --enable-sixel && make
+sudo make install
+cd ..
 
 echo "Setting up Rust"
 rustup default stable
