d614dc23dc74cee5ba056b52b303c4e25cc7e79b
tsac <tsakonas.constantinos@gmail.com>
2024-09-04 03:41:23 +0300

    Clear wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -12,19 +12,16 @@ if wezterm.config_builder then
 end
 
 config.automatically_reload_config = true
-config.front_end = "Software"
 config.term = "xterm-256color"
-local colors, color_conf, window_conf = dofile(os.getenv('HOME') .. '/.config/wezterm/colors.lua')
 
 -- Window
-config.font = wezterm.font_with_fallback {
-    { family = 'JetBrainsMono Nerd Font', weight = 'Medium' },
-    { family = 'Hack Nerd Font', weight = 'Bold' },
-    }
-config.freetype_load_target = "Light"
--- config.colors = color_conf
--- config.color_scheme = 'Modus-Vivendi-Deuteranopia'
-config.color_scheme = 'Moonfly (Gogh)'
+config.font = wezterm.font('FiraCode Nerd Font', {weight="Regular", stretch="Normal", style="Normal"})
+config.front_end = "WebGpu"
+config.freetype_load_flags = 'NO_HINTING'
+config.freetype_render_target = 'HorizontalLcd'
+
+config.bold_brightens_ansi_colors = false
+config.color_scheme = 'Jellybeans (Gogh)'
 config.cursor_blink_rate = 10
 
 -- Tab Bar
@@ -33,42 +30,27 @@ wezterm.on('update-right-status', function(window, pane)
 end)
 config.tab_bar_at_bottom = false
 config.hide_tab_bar_if_only_one_tab = true
-config.use_fancy_tab_bar = false
+config.use_fancy_tab_bar = true
 config.tab_max_width = 100
 config.show_tab_index_in_tab_bar = false
 config.show_new_tab_button_in_tab_bar = false
 
-
 -- Frame
-config.window_frame = window_conf
 config.check_for_updates = true
 config.window_background_opacity = 1.0
 config.text_background_opacity = 1.0
-
--- Show which key table is active in the status area
-wezterm.on('update-right-status', function(window, pane)
-    local name = window:active_key_table()
-    if name then
-	name = 'TABLE: ' .. name
-    end
-    window:set_right_status(name or '')
-end)
-
--- local m = require "lua.mappings"
--- config = mappings(config)
 
 if string.find(io.popen('uname'):read('*a'), 'Darwin') then
     local main_resolution = tonumber(io.popen('system_profiler SPDisplaysDataType | grep Resolution | awk \'{print $2}\' | head -n 1'):read('*a'))
     config.default_prog = {'/opt/homebrew/bin/fish'}
     if main_resolution > 3000 then 
-        config.font_size = 17.5
+        config.font_size = 16
     elseif main_resolution == 1920 then 
-        config.font_size = 13.5
+        config.font_size = 13
     else
-        config.font_size = 12.5
+        config.font_size = 12
     end
     config.window_decorations = "TITLE | RESIZE"
-    -- config.window_decorations = "RESIZE"
 elseif string.find(io.popen('uname'):read('*a'), 'Linux') then
     config.default_prog = {'/usr/bin/fish'}
     config.font_size = 13
@@ -76,4 +58,3 @@ elseif string.find(io.popen('uname'):read('*a'), 'Linux') then
 end
 
 return config
-
