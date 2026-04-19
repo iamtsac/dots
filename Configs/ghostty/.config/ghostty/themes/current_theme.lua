521d63813e706d13389b20867b47c6248de8e969
Constantinos Tsakonas <tsakonas.constantinos@gmail.com>
2026-04-19 16:51:31 +0200

    Restructuring so we can use tuckr instead of stow, in case we dont have root access to the machine

---
+++ Configs/ghostty/.config/ghostty/themes/current_theme.lua
@@ -0,0 +1,26 @@
+local M = {}
+
+M.light = {}
+M.light.color6 = "#005e8b"
+M.light.background = "#ffffff"
+M.light.color1 = "#a60000"
+M.light.color2 = "#006800"
+M.light.color3 = "#6f5500"
+M.light.foreground = "#000000"
+M.light.color5 = "#721045"
+M.light.cursor = "#000000"
+M.light.color7 = "#000000"
+M.light.color8 = "#f2f2f2"
+M.light.color9 = "#d00000"
+M.light.color10 = "#008900"
+M.light.color11 = "#808000"
+M.light.color12 = "#0000ff"
+M.light.color13 = "#dd22dd"
+M.light.color14 = "#008899"
+M.light.color15 = "#595959"
+M.light.color0 = "#ffffff"
+M.light.color4 = "#0031a9"
+M.light.selection_foreground = "#000000"
+M.light.selection_background = "#dfa0f0"
+
+return M
