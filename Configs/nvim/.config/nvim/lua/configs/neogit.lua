e10b8cd2a2e7b5b44049a3003bcead2607d4dfda
Constantinos Tsakonas <tsakonas.constantinos@gmail.com>
2026-05-17 20:22:59 +0200

    Updated views in neogit

--- Configs/nvim/.config/nvim/lua/configs/neogit.lua
+++ Configs/nvim/.config/nvim/lua/configs/neogit.lua
@@ -1,3 +1,40 @@
 require("neogit").setup({
-  graph_style = "unicode",
+    graph_style = "unicode",
+    commit_editor = {
+        kind = "tab",
+        staged_diff_split_kind = "vsplit",
+
+    },
+    commit_select_view = {
+        kind = "floating",
+    },
+    commit_view = {
+        kind = "vsplit",
+        verify_commit = vim.fn.executable("gpg") == 1, -- Can be set to true or false, otherwise we try to find the binary
+    },
+    log_view = {
+        kind = "vsplit",
+    },
+    rebase_editor = {
+        kind = "auto",
+    },
+    reflog_view = {
+        kind = "floating",
+    },
+    merge_editor = {
+        kind = "auto",
+    },
+    preview_buffer = {
+        kind = "floating_console",
+    },
+    popup = {
+        kind = "split",
+        show_title = false,
+    },
+    stash = {
+        kind = "floating",
+    },
+    refs_view = {
+        kind = "floating",
+    },
 })
